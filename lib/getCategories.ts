705788d2a2fb938b46601c4d1405a0305ddc73a8
Delba de Oliveira <delba.brown@gmail.com>
2022-07-13 19:20:00 +0100

    Add dummy data

---
+++ lib/getCategories.ts
@@ -0,0 +1,32 @@
+export const getCategories = () => [
+  {
+    name: 'Electronics',
+    slug: 'electronics',
+    count: 30,
+    items: [
+      { name: 'Phones', slug: 'phones', count: 5 },
+      { name: 'Tablets', slug: 'tablets', count: 10 },
+      { name: 'Laptops', slug: 'laptops', count: 15 },
+    ],
+  },
+  {
+    name: 'Clothes',
+    slug: 'clothes',
+    count: 32,
+    items: [
+      { name: 'Tops', slug: 'tops', count: 7 },
+      { name: 'Shorts', slug: 'shorts', count: 13 },
+      { name: 'Shoes', slug: 'shoes', count: 12 },
+    ],
+  },
+  {
+    name: 'Books',
+    slug: 'books',
+    count: 18,
+    items: [
+      { name: 'Fiction', slug: 'fiction', count: 4 },
+      { name: 'Biography', slug: 'biography', count: 6 },
+      { name: 'Education', slug: 'education', count: 8 },
+    ],
+  },
+];
