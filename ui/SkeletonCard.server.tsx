d39f1f2a25477e7a003fd5f3c944e6e2766a3926
Delba de Oliveira <delba.brown@gmail.com>
2022-07-13 19:20:31 +0100

    Add demo UI

---
+++ ui/SkeletonCard.server.tsx
@@ -0,0 +1,7 @@
+export const SkeletonCard = () => (
+  <div className="space-y-2 rounded-xl bg-zinc-800 p-3">
+    <div className="h-14 rounded-lg bg-zinc-700"></div>
+    <div className="h-3 w-11/12 rounded-lg bg-zinc-600"></div>
+    <div className="h-3 w-8/12 rounded-lg bg-zinc-600"></div>
+  </div>
+);
