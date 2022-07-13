bf38a509eb9b068c026d108e422f521390a6f004
Delba de Oliveira <delba.brown@gmail.com>
2022-07-13 20:28:38 +0100

    Update category index page

--- app/(demo)/layouts/[category]/page.server.tsx
+++ app/(demo)/layouts/[category]/page.server.tsx
@@ -1,16 +1,33 @@
 import { getCategories } from '@/lib/getCategories';
 import { Boundary } from '@/ui/Boundary.server';
+import { Counter } from '@/ui/Counter.client';
+import { SkeletonCard } from '@/ui/SkeletonCard.server';
+import { GetServerSideProps } from 'next';
 
-export const getServerSideProps = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { category } = context.params!;
+
   return {
-    props: { categories: getCategories(), date: new Date().toISOString() },
+    props: {
+      category: getCategories().find((x) => x.slug === category),
+    },
   };
 };
-
-export default function Page(props: any) {
+export default function Page({
+  children,
+  category,
+}: {
+  children: React.ReactNode;
+  category: ReturnType<typeof getCategories>[0];
+}) {
   return (
     <Boundary>
-      <div className="text-xl font-medium text-white">sdsd{props.date}</div>
+      <div className="grid grid-cols-3 gap-5">
+        {Array.from({ length: category.count }).map((_, i) => (
+          <SkeletonCard key={i} />
+        ))}
+        <div>{/* <Counter /> */}</div>
+      </div>
     </Boundary>
   );
 }
