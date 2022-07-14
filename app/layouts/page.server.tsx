de67214fd6ab14ac001aaec39e47d89b186560b7
Delba de Oliveira <delba.brown@gmail.com>
2022-07-14 19:24:36 +0100

    Improve layouts home page

--- app/layouts/page.server.tsx
+++ app/layouts/page.server.tsx
@@ -1,18 +1,30 @@
+import { type Category, getCategories } from '@/lib/getCategories';
 import { Boundary } from '@/ui/Boundary.server';
+import { SkeletonCard } from '@/ui/SkeletonCard.server';
+import Link from 'next/link';
 
-export async function getServerSideProps() {
+export const getServerSideProps = () => {
   return {
-    props: {
-      date: new Date().toISOString(),
-    },
+    props: { categories: getCategories() },
   };
-}
+};
 
-export default function Page(props: any) {
+export default function Page({ categories }: { categories: Category[] }) {
   return (
     <Boundary>
-      <div className="text-xl font-medium text-white">
-        1. Select a category.
+      <div className="grid grid-cols-3 gap-x-8 gap-y-10">
+        {categories.map((category) => (
+          <Link
+            key={category.slug}
+            href={`/layouts/${category.slug}`}
+            className="group relative block space-y-3"
+          >
+            <SkeletonCard />
+            <div className="text-sm font-medium text-zinc-300 group-hover:text-zinc-50">
+              {category.name}
+            </div>
+          </Link>
+        ))}
       </div>
     </Boundary>
   );
