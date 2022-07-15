1fe51f216cf979a20ffa3c6153c2c73d131794b2
Delba de Oliveira <delba.brown@gmail.com>
2022-07-15 06:39:48 +0100

    Add a key to the layout home page to place users

--- app/layouts/page.server.tsx
+++ app/layouts/page.server.tsx
@@ -1,6 +1,8 @@
 import { type Category, getCategories } from '@/lib/getCategories';
 import { Boundary } from '@/ui/Boundary.server';
+import Counter from '@/ui/Counter.client';
 import { SkeletonCard } from '@/ui/SkeletonCard.server';
+import { TabNavItem } from '@/ui/TabNavItem';
 import Link from 'next/link';
 
 export const getServerSideProps = () => {
@@ -12,19 +14,59 @@ export const getServerSideProps = () => {
 export default function Page({ categories }: { categories: Category[] }) {
   return (
     <Boundary>
-      <div className="grid grid-cols-3 gap-x-8 gap-y-10">
-        {categories.map((category) => (
-          <Link
-            key={category.slug}
-            href={`/layouts/${category.slug}`}
-            className="group relative block space-y-3"
-          >
-            <SkeletonCard />
-            <div className="text-sm font-medium text-zinc-300 group-hover:text-zinc-50">
-              {category.name}
+      <div className="space-y-10 text-sm text-zinc-400">
+        <div>
+          <div className="text-xl font-medium text-white">Layouts</div>
+        </div>
+
+        <div className="">
+          <div className="grid grid-cols-[124px,1fr] gap-x-6 gap-y-8">
+            <div>
+              <Boundary>{''}</Boundary>
             </div>
-          </Link>
-        ))}
+
+            <div>
+              The{' '}
+              <span className="font-mono font-medium text-white">{`{children}`}</span>{' '}
+              boundary between a parent layout and a nested layout or page.
+            </div>
+
+            <div>
+              <Boundary isHighlighted={true}>{''}</Boundary>
+            </div>
+            <div>
+              A pink border indicates{' '}
+              <span className="font-mono font-medium text-white">{`{children}`}</span>{' '}
+              have recently re-rendered.
+            </div>
+
+            <div>
+              <Counter />
+            </div>
+
+            <div>
+              A client-side component with state. Use it to highlight that state
+              is preserved in a parent layout when{' '}
+              <span className="font-mono font-medium text-white">{`{children}`}</span>{' '}
+              change.
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <div>Navigate to a category to get started:</div>
+
+          <div className="flex space-x-3">
+            {categories.map((category) => (
+              <TabNavItem
+                key={category.slug}
+                href={`/layouts/${category.slug}`}
+              >
+                {category.name}
+              </TabNavItem>
+            ))}
+          </div>
+        </div>
       </div>
     </Boundary>
   );
--- ui/Boundary.server.tsx
+++ ui/Boundary.server.tsx
@@ -1,9 +1,22 @@
+import clsx from 'clsx';
 import React from 'react';
 
-export const Boundary = ({ children }: { children: React.ReactNode }) => {
+export const Boundary = ({
+  children,
+  isHighlighted,
+}: {
+  children: React.ReactNode;
+  isHighlighted?: boolean;
+}) => {
   return (
-    <div className="relative animate-[childrenRender_1s_ease-in-out_1] rounded-xl border border-dashed border-white/30 p-7">
-      <div className="absolute -top-2.5 left-6 rounded-full border-2 border-black bg-zinc-800 px-1.5 text-[10px] tracking-wider text-zinc-500">
+    <div
+      className={clsx(
+        'relative animate-[childrenRender_1s_ease-in-out_1] rounded-xl border border-dashed p-7',
+        { 'border-zinc-600': !isHighlighted },
+        { 'border-pink-600': isHighlighted },
+      )}
+    >
+      <div className="absolute -top-2.5 left-6 rounded-full border-2 border-black bg-zinc-800 px-1.5 text-[10px] leading-4 tracking-wider text-zinc-500">
         CHILDREN
       </div>
       {children}
