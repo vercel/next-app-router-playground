8421bdc9ff50396e4c0c774a3b2074eeabe23626
Delba de Oliveira <delba.brown@gmail.com>
2022-07-20 10:47:01 +0100

    Add soft push explanation

--- app/soft-push/page.server.tsx
+++ app/soft-push/page.server.tsx
@@ -46,7 +46,34 @@ export default function Page({ categories }: { categories: Category[] }) {
       <div className="space-y-4">
         <div className="text-white">Notes</div>
         <ul className="list-disc space-y-2 pl-4 text-sm text-zinc-300">
-          <li>To do</li>
+          <li>This example temporarily requires a production build.</li>
+          <li>
+            As you navigate around the app, the router stores the result of the
+            React Server Component payload in an in-memory client-side cache.
+          </li>
+          <li>
+            The navigation items in this example use{' '}
+            <span className="font-medium text-white">{`<Link soft>`}</span>. On
+            soft navigation, segments that change use the cache (if it exists),
+            and no requests to the server are made. Unchanged segments are
+            preserved because they do not re-render.
+          </li>
+          <li>
+            Try navigating to a category to add it to the cache. Notice it takes
+            time to load. Then navigate away and back again. Notice this time
+            navigation is instant because it uses the cache.
+          </li>
+          <li>
+            Back/Forward navigation uses soft navigation by default. Try
+            navigating backwards or forwards after visiting all categories for
+            instant navigation.
+          </li>
+          <li>
+            Links to the last category uses a hard push which is the default for{' '}
+            <span className="font-medium text-white">{`<Link>`}</span>.
+            Navigating to this item will always invalidate the cache and refetch
+            from the server.
+          </li>
         </ul>
       </div>
     </div>
