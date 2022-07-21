1c4f2072c72b15e33ae4156eba2b8a9db149221c
Delba de Oliveira <delba.brown@gmail.com>
2022-07-21 11:42:54 +0100

    Add other Vercel colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         vercel: {
           pink: '#FF0080',
           blue: '#0070F3',
+          cyan: '#50E3C2',
+          orange: '#F5A623',
+          violet: '#7928CA',
         },
       },
       keyframes: ({ theme }) => ({
