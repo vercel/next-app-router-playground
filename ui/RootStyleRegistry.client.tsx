ffa521d1b350f37d286e5ebdca21353ff6c5d081
Jiachi Liu <inbox@huozhi.im>
2022-08-10 18:56:15 +0200

    ui components

---
+++ ui/RootStyleRegistry.client.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+// @ts-ignore
+import { StyleRegistry, createStyleRegistry } from 'styled-jsx'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import { useFlushEffects } from 'next/dist/client/components/hooks-client'
+
+export default function RootStyleRegistry({ children } : { children: JSX.Element }) {
+  const [jsxStyleRegistry] = useState(() => createStyleRegistry())
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
+  const styledJsxFlushEffect = () => {
+    const styles = jsxStyleRegistry.styles()
+    jsxStyleRegistry.flush()
+    return <>{styles}</>
+  }
+  const styledComponentsFlushEffect = () => {
+    const styles = styledComponentsStyleSheet.getStyleElement()
+    styledComponentsStyleSheet.seal()
+    return <>{styles}</>
+  }
+
+  useFlushEffects(() => {
+    return (
+      <>
+        {styledJsxFlushEffect()}
+        {styledComponentsFlushEffect()}
+      </>
+    )
+  })
+
+  // Only include style registry on server side for SSR
+  if (typeof window === 'undefined') {
+    return (
+      <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+        <StyleRegistry registry={jsxStyleRegistry}>{children}</StyleRegistry>
+      </StyleSheetManager>
+    )
+  }
+
+  return children
+}
