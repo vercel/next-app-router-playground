f3f86e43f809abc72818c8616743f2dfe6b1979b
Jiachi Liu <inbox@huozhi.im>
2022-08-10 18:53:57 +0200

    css-in-js

--- app/layout.server.tsx
+++ app/layout.server.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css';
-import AddressBar from '@/ui/AddressBar.client';
-import nextPackageJson from 'next/package.json';
 import React from 'react';
+import AddressBar from '@/ui/AddressBar.client';
+import Header from '@/ui/Header.client';
+import RootStyleRegistry from '@/ui/RootStyleRegistry.client';
+import nextPackageJson from 'next/package.json';
 import GlobalNav from './GlobalNav.client';
+import Footer from '@/ui/Footer.client'
 
 export default function RootLayout({ children }: { children: any }) {
   return (
@@ -10,28 +13,29 @@ export default function RootLayout({ children }: { children: any }) {
       <head>
         <title>Next.js Layouts and Routing Playground</title>
       </head>
-      <body className="overflow-y-scroll bg-zinc-900">
-        <div className="grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-12">
-          <div className="col-start-2">
-            <GlobalNav />
-          </div>
-
-          <div className="col-start-3 space-y-6">
-            <AddressBar />
-
-            <div className="rounded-xl border border-zinc-800 bg-black p-8">
-              {children}
+      <RootStyleRegistry>
+        <body className="overflow-y-scroll bg-zinc-900">
+          <div className="grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-8">
+            <Header />
+            <div className="col-start-2">
+              <GlobalNav />
             </div>
-          </div>
 
-          <div className="col-start-3 col-end-4 mt-28">
-            <div className="flex space-x-6 text-sm text-zinc-600">
-              <div>React: {React.version}</div>
-              <div>Next: {nextPackageJson.version}</div>
+            <div className="col-start-3 space-y-6">
+              <AddressBar />
+
+              <div className="rounded-xl border border-zinc-800 bg-black p-8">
+                {children}
+              </div>
             </div>
+
+            <Footer
+              reactVersion={React.version}
+              nextVersion={nextPackageJson.version}
+            />
           </div>
-        </div>
-      </body>
+        </body>
+      </RootStyleRegistry>
     </html>
   );
 }
