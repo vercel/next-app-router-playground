a412694cfc3d4ef99ea0fec9362fe22e3dff6e8a
Delba de Oliveira <delba.brown@gmail.com>
2022-08-16 13:39:20 +0100

    Update example description

--- app/styling/page.server.tsx
+++ app/styling/page.server.tsx
@@ -2,15 +2,24 @@ export default function Page() {
   return (
     <div className="space-y-4">
       <div className="text-white">Notes</div>
+
       <ul className="list-disc space-y-2 pl-4 text-sm text-zinc-300">
         <li>
-          This example uses different styling solutions to style each page.
+          Supporting CSS-in-JS (Styled Components and Styled JSX) is a
+          three-step opt-in process that involves:
         </li>
-        <li>TODO: Discuss</li>
         <ul className="list-disc space-y-2 pl-4 text-sm text-zinc-300">
-          <li>`useFlushEffect`</li>
-          <li>Client-side requirements</li>
-          <li>Challenges posed by streaming and partial rendering</li>
+          <li>
+            Creating a "style registry" to collect all CSS rules in a render.
+          </li>
+          <li>
+            Using the new `useFlushEffects` hook to inject rules before any
+            content that might use them.
+          </li>
+          <li>
+            Creating a client component that wraps your app with the style
+            registry during initial server-side rendering.
+          </li>
         </ul>
       </ul>
     </div>
