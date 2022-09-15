ed49e6e1b6a6172563a6cfa389eb3390990ea8dd
Delba de Oliveira <delbabrown@gmail.com>
2022-09-15 07:51:29 +0100

    Simplify home demo home pages

--- app/loading/page.server.tsx
+++ app/loading/page.server.tsx
@@ -1,38 +1,10 @@
-import { getCategories, type Category } from '@/lib/getCategories';
 import { Boundary } from '@/ui/Boundary.server';
-import { SectionLink } from '@/ui/SectionLink.server';
-import { SkeletonCard } from '@/ui/SkeletonCard.server';
 
-export const getServerSideProps = () => {
-  return {
-    props: { categories: getCategories() },
-  };
-};
-
-export default function Page({ categories }: { categories: Category[] }) {
+export default function Page() {
   return (
     <div className="space-y-9">
       <Boundary>
-        <div className="space-y-4">
-          <div className="text-xl font-medium text-zinc-500">Home</div>
-          <div className="grid grid-cols-3 gap-6">
-            {categories.map((category) => {
-              return (
-                <SectionLink
-                  key={category.slug}
-                  href={`/loading/${category.slug}`}
-                  text={category.name}
-                >
-                  <SkeletonCard />
-                </SectionLink>
-              );
-            })}
-
-            <SectionLink href="/loading/no-loading" text="No Loading UI">
-              <SkeletonCard />
-            </SectionLink>
-          </div>
-        </div>
+        <div className="text-xl font-medium text-zinc-500">Home</div>
       </Boundary>
 
       <div className="space-y-4">
