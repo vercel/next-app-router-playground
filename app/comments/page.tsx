e4b900272df7a8896570c793c9cfd3921f233198
Michael Novotny <manovotny@gmail.com>
2022-10-25 00:14:02 -0700

    Adds comment FYI link.

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -4,7 +4,10 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center">
       <div className="text-3xl font-bold text-white">
-        <p className="mb-10">Scan this QR code to leave comments</p>
+        <p className="mb-10">
+          Visit: https://vercel.fyi/nextjs-conf-demo-comments
+        </p>
+        <p className="mb-10">Or scan this QR code to leave comments 👇</p>
         <Image
           className="border-5 m-0 m-0 m-auto rounded-lg border-4 border-gray-300 bg-white p-10"
           src="/qr-comments.jpg"
