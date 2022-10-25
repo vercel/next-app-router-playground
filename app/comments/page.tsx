aece75eb0249c67706213ef6d5340d28c8a687b7
Michael Novotny <manovotny@gmail.com>
2022-10-24 22:00:16 -0700

    Adds QR code for comments.

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center">
       <div className="text-3xl font-bold text-white">
-        Scan this QR code to leave comments
+        <p className="mb-10">Scan this QR code to leave comments</p>
+        <Image
+          className="border-5 m-0 m-0 m-auto rounded-lg border-4 border-gray-300 bg-white p-10"
+          src="/qr-comments.jpg"
+          alt="QR code to try Vercel Comments"
+          width={500}
+          height={500}
+        />
       </div>
     </div>
   );
