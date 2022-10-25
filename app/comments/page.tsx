9762f2f2e5d46fde7c7185311cb4001fc4078e52
Michael Novotny <manovotny@gmail.com>
2022-10-25 00:16:33 -0700

    Fixes comment styles.

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <div className="flex items-center justify-center">
-      <div className="text-3xl font-bold text-white">
-        <p className="mb-10">
-          Visit: https://vercel.fyi/nextjs-conf-demo-comments
+      <div className="text-center text-3xl font-bold text-white">
+        <p className="m-auto mb-10">vercel.fyi/nextjs-conf-demo-comments</p>
+        <p className="m-auto mb-10">
+          Or scan this QR code to leave comments 👇
         </p>
-        <p className="mb-10">Or scan this QR code to leave comments 👇</p>
         <Image
-          className="border-5 m-0 m-0 m-auto rounded-lg border-4 border-gray-300 bg-white p-10"
+          className="border-5 m-auto rounded-lg border-4 border-gray-300 bg-white p-10"
           src="/qr-comments.jpg"
           alt="QR code to try Vercel Comments"
           width={500}
