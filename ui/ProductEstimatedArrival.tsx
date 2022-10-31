9b19b90ffbb7ed1f1b1c9a5d5d2e0b132694cd43
Delba de Oliveira <delbabrown@gmail.com>
2022-10-31 13:28:32 +0000

    Small fixes

--- ui/ProductEstimatedArrival.tsx
+++ ui/ProductEstimatedArrival.tsx
@@ -1,6 +1,12 @@
 import { add, format, isTomorrow } from 'date-fns';
 
-export const ProductEstimatedArrival = ({ leadTime }: { leadTime: number }) => {
+export const ProductEstimatedArrival = ({
+  leadTime,
+  hasDeliveryTime = false,
+}: {
+  leadTime: number;
+  hasDeliveryTime?: boolean;
+}) => {
   const date = add(new Date(), {
     days: leadTime,
   });
@@ -8,10 +14,11 @@ export const ProductEstimatedArrival = ({ leadTime }: { leadTime: number }) => {
   return (
     <div className="text-sm text-zinc-300">
       Get it{' '}
-      <strong className="font-bold">
+      <strong className="font-bold text-zinc-100">
         {isTomorrow(date) ? 'tomorrow, ' : null}
         {format(date, 'MMM d')}
       </strong>
+      {hasDeliveryTime ? <> by 5pm</> : null}
     </div>
   );
 };
