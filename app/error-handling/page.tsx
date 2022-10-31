342708b6358678b0f1a6d180e3b61949d289f89c
wishee <84540554+vaishnav-mk@users.noreply.github.com>
2022-10-31 17:54:49 +0300

    Fixes error handling "Learn More" link. (#54)

--- app/error-handling/page.tsx
+++ app/error-handling/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
       <div>
         <a
           className="font-medium text-zinc-300 hover:text-white"
-          href="https://beta.nextjs.org/docs/routing/loading-ui"
+          href="https://beta.nextjs.org/docs/routing/error-handling"
         >
           Learn more
         </a>
