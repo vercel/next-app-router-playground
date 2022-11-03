db019cac87ed5610b46896b9884e84651ab84435
StephDietz <steph.dietz@vercel.com>
2022-11-03 12:40:30 -0500

    add aria label to search input and meta tags to improve SEO/ accessability scores

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,15 @@ import GlobalNav from './GlobalNav';
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <html>
+    <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Next.js App Directory Playground</title>
+        <meta
+          name="description"
+          content="Next.js App Directory Playground"
+          key="desc"
+        />
       </head>
       <body className="overflow-y-scroll bg-zinc-900">
         <div className="grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-8">
@@ -17,12 +23,12 @@ export default function RootLayout({ children }: { children: any }) {
           <div className="col-start-3 space-y-6">
             <AddressBar />
 
-            <div className="rounded-xl border border-zinc-800 bg-black p-8">
+            <div className="p-8 bg-black border rounded-xl border-zinc-800">
               {children}
             </div>
           </div>
 
-          <div className="col-start-3 col-end-4 mt-28 flex items-center justify-center">
+          <div className="flex items-center justify-center col-start-3 col-end-4 mt-28">
             <div className="text-sm text-zinc-600">
               Created by the <b>Next.js</b>
               {' team at '}
--- app/streaming/layout.tsx
+++ app/streaming/layout.tsx
@@ -9,7 +9,7 @@ async function ShoppingCartCount() {
   await delay(1500);
 
   return (
-    <div className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-vercel-pink text-sm font-bold text-white">
+    <div className="absolute flex items-center justify-center w-4 h-4 text-sm font-bold text-white rounded-full -top-1 -right-1 bg-vercel-pink">
       <span>3</span>
     </div>
   );
@@ -18,9 +18,9 @@ async function ShoppingCartCount() {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="space-y-16">
-      <div className="flex items-center justify-between rounded-lg bg-zinc-800 px-5 py-4">
+      <div className="flex items-center justify-between px-5 py-4 rounded-lg bg-zinc-800">
         <div className="flex space-x-4">
-          <div className="h-10 w-10">
+          <div className="w-10 h-10">
             <Image
               src={logo}
               className="rounded-full"
@@ -30,21 +30,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
 
           <div className="relative w-56">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <SearchIcon className="h-5 w-5 text-zinc-300" />
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <SearchIcon className="w-5 h-5 text-zinc-300" />
             </div>
             <input
+              aria-label="Search"
               type="search"
               name="search"
               id="search"
-              className="block w-full rounded-full border-none bg-zinc-600 pl-10 font-medium text-zinc-200 focus:border-vercel-pink focus:ring-2 focus:ring-vercel-pink"
+              className="block w-full pl-10 font-medium border-none rounded-full bg-zinc-600 text-zinc-200 focus:border-vercel-pink focus:ring-2 focus:ring-vercel-pink"
               autoComplete="off"
             />
           </div>
         </div>
 
         <div className="flex space-x-3">
-          <div className="relative flex h-10 w-10 items-center justify-center rounded-full bg-zinc-600 text-white">
+          <div className="relative flex items-center justify-center w-10 h-10 text-white rounded-full bg-zinc-600">
             <ShoppingCartIcon className="w-6 text-white" />
             <Suspense>
               {/* @ts-ignore */}
