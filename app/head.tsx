1b37d21afbcd2b337edff790a9fb2f5d0e8786c5
Delba de Oliveira <delbabrown@gmail.com>
2022-11-10 15:11:44 +0000

    Redesign home page, root layout and navigation

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Next.js 13: App Directory Playground</title>
+      <meta
+        name="description"
+        content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
+      />
+    </>
+  );
+}
