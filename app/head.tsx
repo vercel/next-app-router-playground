fc76d64a7ab2a53abcdd5f1e2ba89abb6dc94bc6
Delba de Oliveira <delbabrown@gmail.com>
2022-11-18 13:29:50 +0000

    Add component to handle default head tags

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,13 @@
+import { DefaultHead } from '#/ui/DefaultHead';
+
 export default function Head() {
   return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Next.js 13: App Directory Playground</title>
+    <DefaultHead>
+      <title>Next.js App Directory Playground</title>
       <meta
         name="description"
         content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
       />
-    </>
+    </DefaultHead>
   );
 }
