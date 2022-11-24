e77cefd3497d565054293de2758005d3828f8b20
Ismael Martínez <hola@isma.uy>
2022-11-24 05:47:56 -0300

    Update tailwind.config.js (#77)
    Despite is not used in these examples, tailwind configuration is not working with the pages directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './page/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
     './ui/**/*.{js,ts,jsx,tsx}',
   ],
   future: {
