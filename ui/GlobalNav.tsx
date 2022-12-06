92cb8b8992f805ea2934aadc582077ebfe33f549
Delba de Oliveira <delbabrown@gmail.com>
2022-12-06 12:48:45 +0000

    Remove accidental import

--- ui/GlobalNav.tsx
+++ ui/GlobalNav.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { demos, type Item } from '#/lib/demos';
-import '#/styles/globals.css';
 import { NextLogo } from '#/ui/NextLogo';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
