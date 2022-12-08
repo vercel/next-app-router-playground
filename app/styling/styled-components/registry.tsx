402a62081ead8d14ec461126e9ac385a6adb3256
Jiachi Liu <inbox@huozhi.im>
2022-12-08 20:56:04 +0100

    fix typing

--- app/styling/styled-components/registry.tsx
+++ app/styling/styled-components/registry.tsx
@@ -19,7 +19,7 @@ export default function StyledComponentsRegistry({
     return <>{styles}</>;
   });
 
-  if (typeof window !== 'undefined') return children;
+  if (typeof window !== 'undefined') return <>{children}</>;
 
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
