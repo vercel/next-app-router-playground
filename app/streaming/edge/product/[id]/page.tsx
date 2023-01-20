de2913c6be7f0303ea01bfb65f569bfc8ef6c414
Delba de Oliveira <delbabrown@gmail.com>
2023-01-20 08:18:25 +0000

    Split streaming into two runtimes

---
+++ app/streaming/edge/product/[id]/page.tsx
@@ -0,0 +1,50 @@
+import type { Product } from '#/types/Product';
+import { Ping } from '#/ui/Ping';
+import { Suspense } from 'react';
+import {
+  RecommendedProducts,
+  RecommendedProductsSkeleton,
+} from '../../../_components/RecommendedProducts';
+import { Reviews, ReviewsSkeleton } from '../../../_components/Reviews';
+import { SingleProduct } from '../../../_components/SingleProduct';
+
+export const runtime = 'experimental-edge';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const data = await fetch(
+    // you would normally fetch data from an external data source
+    `https://app-dir.vercel.app/api/products?id=${params.id}`,
+  );
+  const product = (await data.json()) as Product;
+
+  return (
+    <div className="space-y-8 lg:space-y-14">
+      <SingleProduct product={product} />
+
+      <div className="relative">
+        <div className="absolute top-2 -left-4">
+          <Ping />
+        </div>
+      </div>
+
+      <Suspense fallback={<RecommendedProductsSkeleton />}>
+        {/* @ts-expect-error Async Server Component */}
+        <RecommendedProducts
+          path="/streaming/edge/product"
+          productId={params.id}
+        />
+      </Suspense>
+
+      <div className="relative">
+        <div className="absolute top-2 -left-4">
+          <Ping />
+        </div>
+      </div>
+
+      <Suspense fallback={<ReviewsSkeleton />}>
+        {/* @ts-expect-error Async Server Component */}
+        <Reviews />
+      </Suspense>
+    </div>
+  );
+}
--- app/streaming/layout.tsx
+++ app/streaming/layout.tsx
@@ -1,63 +1,35 @@
-import { NextLogo } from '#/ui/NextLogo';
-import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/solid';
-import { cookies } from 'next/headers';
-import Image from 'next/image';
-import Link from 'next/link';
+import { TabGroup } from '#/ui/TabGroup';
 import React from 'react';
-import { CartCount } from './CartCount';
-import { CartCountProvider } from './CartCountContext';
 
-export const runtime = 'experimental-edge';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  const cartCount = Number(cookies().get('_cart_count')?.value || '0');
-
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <CartCountProvider initialCartCount={cartCount}>
-      <div className="space-y-12 lg:space-y-16">
-        <div className="flex items-center justify-between gap-x-3 rounded-lg bg-gray-800 px-3 py-3 lg:px-5 lg:py-4">
-          <div className="flex gap-x-3">
-            <Link href="/streaming">
-              <div className="h-10 w-10">
-                <NextLogo />
-              </div>
-            </Link>
-
-            <div className="relative flex-1">
-              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                <SearchIcon className="h-5 w-5 text-gray-300" />
-              </div>
-              <input
-                aria-label="Search"
-                type="search"
-                name="search"
-                id="search"
-                className="block w-full rounded-full border-none bg-gray-600 pl-10 font-medium text-gray-200 focus:border-vercel-pink focus:ring-2 focus:ring-vercel-pink"
-                autoComplete="off"
-              />
-            </div>
-          </div>
-
-          <div className="flex shrink-0 gap-x-3">
-            <div className="relative flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-gray-600 text-white">
-              <ShoppingCartIcon className="w-6 text-white" />
-              <div className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-vercel-pink text-sm font-bold text-white">
-                <CartCount />
-              </div>
-            </div>
-
-            <Image
-              src="/prince-akachi-LWkFHEGpleE-unsplash.jpg"
-              className="rounded-full"
-              width={40}
-              height={40}
-              alt="User"
-            />
-          </div>
-        </div>
-
-        <div>{children}</div>
+    <div className="space-y-9">
+      <div className="flex justify-between">
+        <TabGroup
+          path="/streaming"
+          items={[
+            {
+              text: 'Home',
+            },
+            {
+              text: 'Edge Runtime',
+              slug: 'edge/product/1',
+              segment: 'edge',
+            },
+            {
+              text: 'Node Runtime',
+              slug: 'node/product/1',
+              segment: 'node',
+            },
+          ]}
+        />
       </div>
-    </CartCountProvider>
+
+      <div>{children}</div>
+    </div>
   );
 }
