1fd361bc6fb46741fae7df6c8f7c8319eb2cd9d0
JJ Kasper <jj@jjsweb.site>
2023-01-26 11:19:03 -0800

    remove force-dynamic usage

--- app/streaming/node/layout.tsx
+++ app/streaming/node/layout.tsx
@@ -4,14 +4,12 @@ import React from 'react';
 import { CartCountProvider } from '../_components/CartCountContext';
 import { Header } from '../_components/Header';
 
-export const dynamic = 'force-dynamic';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   const cartCount = Number(cookies().get('_cart_count')?.value || '0');
 
   return (
     <>
-      <ul className="list-disc space-y-2 pl-4 text-sm text-gray-300 mb-12">
+      <ul className="mb-12 list-disc space-y-2 pl-4 text-sm text-gray-300">
         <li>
           Primary product information is loaded first as part of the initial
           response.
