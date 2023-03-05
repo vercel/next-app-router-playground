90c70c8b93f39ff08a8f17424af87ac95ea99ed1
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-03-05 16:50:01 +0000

    Update global-nav.tsx (#115)

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -25,7 +25,7 @@ export function GlobalNav() {
           </div>
 
           <h3 className="font-semibold tracking-wide text-gray-400 group-hover:text-gray-50">
-            App Directory <span className="Work in progress">(WIP)</span>
+            App Router <span className="Work in progress">(WIP)</span>
           </h3>
         </Link>
       </div>
