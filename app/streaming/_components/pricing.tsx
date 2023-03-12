4c09d40d194477ff0b3588ed1a3772a7d5f2df20
Malte Ubl <cramforce@users.noreply.github.com>
2023-03-12 16:00:31 -0700

    Do less Next Cache so we can demo streaming (#116)
    * Do less Next Cache so we can demo streaming
    
    * Undo extra streaming

--- app/streaming/_components/pricing.tsx
+++ app/streaming/_components/pricing.tsx
@@ -30,6 +30,11 @@ function LoadingDots() {
 async function UserSpecificDetails({ productId }: { productId: string }) {
   const data = await fetch(
     `https://app-dir.vercel.app/api/products?id=${productId}&delay=500&filter=price,usedPrice,leadTime,stock`,
+    {
+      // We intentionally disable Next.js Cache to better demo
+      // streaming
+      cache: 'no-store',
+    },
   );
 
   const product = (await data.json()) as Product;
