58df2c1f16c9099c7d8e8a9264d8c90794489f5b
Steven Tey <stevensteel97@gmail.com>
2023-03-15 14:39:46 -0500

    fixed border-b

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -14,7 +14,7 @@ export function GlobalNav() {
   const close = () => setIsOpen(false);
 
   return (
-    <div className="fixed top-0 z-10 flex w-full flex-col border-b border-gray-800 bg-black lg:bottom-0 lg:z-auto lg:w-72 lg:border-r lg:border-gray-800">
+    <div className="fixed top-0 z-10 flex w-full flex-col border-b border-gray-800 bg-black lg:bottom-0 lg:z-auto lg:w-72 lg:border-b-0 lg:border-r lg:border-gray-800">
       <div className="flex h-14 items-center py-4 px-4 lg:h-auto">
         <Link
           href="/"
