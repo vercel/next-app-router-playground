c497a65f3fe5f67242a48997c3dc49012b442869
Delba de Oliveira <delba.brown@gmail.com>
2023-04-05 17:58:34 +0100

    simpler example

--- app/parallel-routes/layout.tsx
+++ app/parallel-routes/layout.tsx
@@ -1,10 +1,8 @@
-import { Boundary } from '#/ui/boundary';
-
 export default function Layout({ children, slot }) {
   return (
     <>
       {children}
-      <Boundary labels={['slot']}>{slot}</Boundary>
+      {slot}
     </>
   );
 }
