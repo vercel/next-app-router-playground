afd269f3a52f7419c75f8883e2e9914612115fe0
Max Kayander <maxim.kayander1@gmail.com>
2023-05-03 17:27:09 +0200

    Don't use deprecated "ReactChild" type

--- app/styling/styled-components/registry.tsx
+++ app/styling/styled-components/registry.tsx
@@ -23,7 +23,7 @@ export default function StyledComponentsRegistry({
 
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      {children as React.ReactChild}
+      {children as JSX.Element}
     </StyleSheetManager>
   );
 }
