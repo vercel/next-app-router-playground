3f9bbff377fadbf150ad05bcf4ab6bede7e396cc
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2023-11-14 17:35:10 +0000

    Update Next.js to 14 (#152)
    * Update next to 14
    
    * Update next/og import
    
    * remove experimental flags

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
