2f1d34bd5110a3cbaadad1097aecdf1dc86eab32
Delba de Oliveira <delbabrown@gmail.com>
2024-04-24 17:47:40 +0100

    Fix svg attributes

--- ui/next-logo.tsx
+++ ui/next-logo.tsx
@@ -74,7 +74,7 @@ export function NextLogoDark() {
           r="87"
           fill="black"
           stroke="white"
-          stroke-width="6"
+          strokeWidth="6"
         />
         <path
           d="M149.508 157.52L69.142 54H54V125.97H66.1136V69.3836L139.999 164.845C143.333 162.614 146.509 160.165 149.508 157.52Z"
@@ -97,8 +97,8 @@ export function NextLogoDark() {
           y2="160.5"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="white" stop-opacity="0" />
+          <stop stopColor="white" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_408_139"
@@ -108,8 +108,8 @@ export function NextLogoDark() {
           y2="106.875"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="white" stop-opacity="0" />
+          <stop stopColor="white" />
+          <stop offset="1" stop-color="white" stopOpacity="0" />
         </linearGradient>
       </defs>
     </svg>
