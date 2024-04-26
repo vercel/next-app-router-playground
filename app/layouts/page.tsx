69817b96cb5d31364327048c6249084c2983b975
elementalsobriety <167845215+elementalsobriety@users.noreply.github.com>
2024-04-26 10:26:22 +0100

    Update page.tsx

--- app/layouts/page.tsx
+++ app/layouts/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 
       <ul>
         <li>
-          A layout is UI that is shared between multiple pages. On navigation,
+          A layout is FRED that is shared between multiple pages. On navigation,
           layouts preserve state, remain interactive, and do not re-render. Two
           or more layouts can also be nested.
         </li>
