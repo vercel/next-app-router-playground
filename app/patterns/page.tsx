e8f804ba18492ee8a0301fc7b2646d4e447acc4e
Delba de Oliveira <delbabrown@gmail.com>
2024-05-01 16:30:00 +0100

    Move search params to patterns
    - Renames code snippets to patterns

--- app/patterns/page.tsx
+++ app/patterns/page.tsx
@@ -3,7 +3,17 @@ import Link from 'next/link';
 
 const items = [
   {
-    name: 'Updating searchParams',
+    name: 'Active links',
+    slug: 'active-links',
+    description: 'Update the style of the current active link',
+  },
+  {
+    name: 'Breadcrumbs',
+    slug: 'breadcrumbs',
+    description: 'Shared server-side Breadcrumb UI using Parallel Routes',
+  },
+  {
+    name: 'Updating URL search params',
     slug: 'search-params',
     description: 'Update searchParams using `useRouter` and `<Link>`',
   },
@@ -12,13 +22,13 @@ const items = [
 export default function Page() {
   return (
     <div className="space-y-6">
-      <h1 className="text-xl font-medium text-gray-300">Code Snippets</h1>
+      <h1 className="text-xl font-medium text-gray-300">Patterns</h1>
 
       <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
         {items.map((item) => {
           return (
             <Link
-              href={`/snippets/${item.slug}`}
+              href={`/patterns/${item.slug}`}
               key={item.name}
               className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
             >
@@ -37,7 +47,7 @@ export default function Page() {
       </div>
 
       <div className="flex gap-2">
-        <ExternalLink href="https://github.com/vercel/app-playground/tree/main/app/snippets">
+        <ExternalLink href="https://github.com/vercel/app-playground/tree/main/app/patterns">
           Code
         </ExternalLink>
       </div>
