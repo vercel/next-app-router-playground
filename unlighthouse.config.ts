b273e478b24f1d0451c98269e9bdec31a06264bc
Abhijeet Tilekar <abhijeet.tilekar@bluepineapple.io>
2024-05-23 15:02:05 +0530

    - Update git action config

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,22 +1,22 @@
 export default {
   site: 'https://www.drive.com.au/',
   urls: [
-    // '/about-drive/',
     '/cars-for-sale/',
     '/showrooms/',
     '/drive-car-of-the-year/2024/'
    ],
   // debug: true,
-  puppeteerClusterOptions: {
-    // only run 1 worker at a time
-    // maxConcurrency: 1
-  },
+  // puppeteerClusterOptions: {
+  //   only run 1 worker at a time
+  //   maxConcurrency: 1
+  // },
   scanner: {
     device: 'desktop',
     throttle: false,
     samples: 3,
   },
+  // Exclude performance scores since they are not correct.
   lighthouseOptions: {
-    // onlyCategories: ['seo'],
+    onlyCategories: ['seo','accessibility','best-practices'],
   }
 }
