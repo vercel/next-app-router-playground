1abc1b03d4a054d9aba830ec7eb66d00f9a4fdf8
Abhijeet Tilekar <abhijeet.tilekar@bluepineapple.io>
2024-05-24 12:37:48 +0530

    - Update git action config

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -10,14 +10,14 @@ export default {
     '/drive-car-of-the-year/2024/'
    ],
   // debug: true,
-  puppeteerClusterOptions: {
-  //   only run 1 worker at a time
+  // puppeteerClusterOptions: {
+  // //   only run 1 worker at a time
   //   maxConcurrency: 1
-  },
+  // },
   scanner: {
     device: 'desktop',
     throttle: false,
-    samples: 10,
+    samples: 5,
   },
   // Exclude performance scores since they are not correct.
   lighthouseOptions: {
