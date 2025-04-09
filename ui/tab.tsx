44b9eb981521d45e1944c65b0fac3e38a77576fd
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2025-04-09 14:27:55 +0100

    Add pending indicator to category subnav

--- ui/tab.tsx
+++ ui/tab.tsx
@@ -2,7 +2,7 @@
 
 import type { Item } from '#/ui/tab-group';
 import clsx from 'clsx';
-import Link from 'next/link';
+import Link, { useLinkStatus } from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export const Tab = ({ path, item }: { path: string; item: Item }) => {
@@ -15,6 +15,8 @@ export const Tab = ({ path, item }: { path: string; item: Item }) => {
   );
 };
 
+// Note: We create an additional component because useLinkStatus should be
+// called from a component that is rendered inside a `<Link>`
 function TabContent({
   children,
   href,
@@ -24,13 +26,15 @@ function TabContent({
 }) {
   const pathname = usePathname();
   const isActive = pathname === href;
+  const { pending: isPending } = useLinkStatus();
 
   return (
     <span
-      className={clsx('flex rounded-md px-3 py-1', {
+      className={clsx('flex rounded-md px-3 py-1 transition duration-75', {
         'bg-gray-700 text-gray-100 hover:bg-gray-500 hover:text-white':
-          !isActive,
+          !isActive && !isPending,
         'bg-vercel-blue text-white': isActive,
+        'bg-gray-800 text-gray-500 delay-75': isPending,
       })}
     >
       {children}
