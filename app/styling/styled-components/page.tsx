2d5e976ccfd1f2c66020a80c8c992458368669ac
Hernan Alvarado <hernanvid123@gmail.com>
2025-04-16 04:51:24 -0500

    chore: improve styles in CSS and CSS-in-JS (#178)

--- app/styling/styled-components/page.tsx
+++ app/styling/styled-components/page.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, minmax(0, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(9.5rem, 1fr));
   gap: 1.5rem /* 24px */;
 `;
 
