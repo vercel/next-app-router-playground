fdac0a46ca5390541360721bf13183e29aaac0b9
Maia Teegarden <dev@padmaia.rocks>
2025-05-19 20:49:31 -0700

    wrap /use-link-status page in suspense

--- app/use-link-status/page.tsx
+++ app/use-link-status/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { getProducts } from '#/app/_internal/data';
 import { Boundary } from '#/ui/boundary';
 import { ProductCard } from '#/ui/new/product-card';
@@ -15,20 +16,22 @@ export default async function Page() {
   const products = getProducts({ limit: 9 });
 
   return (
-    <Boundary label="page.tsx">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-xl font-semibold text-gray-300">
-          All{' '}
-          <span className="font-mono tracking-tighter text-gray-600">
-            ({products.length})
-          </span>
-        </h1>
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+    <Suspense>
+      <Boundary label="page.tsx">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-xl font-semibold text-gray-300">
+            All{' '}
+            <span className="font-mono tracking-tighter text-gray-600">
+              ({products.length})
+            </span>
+          </h1>
+          <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         </div>
-      </div>
-    </Boundary>
+      </Boundary>
+    </Suspense>
   );
 }
