37c2fdb945c89723dcb92ca9849ac994c177a663
Delba de Oliveira <32464864+delbaoliveira@users.noreply.github.com>
2025-06-07 11:17:29 +0100

    Constrain uselinkstatus demo to sub categories

--- app/use-link-status/page.tsx
+++ app/use-link-status/page.tsx
@@ -7,10 +7,8 @@ export default async function Page() {
   // DEMO:
   // This page would normally be prerendered at build time because it doesn't use dynamic APIs.
   // That means the loading state wouldn't show. To force one:
-  // 1. We indicate that we require a user Request before continuing:
+  // We indicate that we require a user Request before continuing:
   await connection();
-  // 2. Add an artificial delay to make the loading state more noticeable:
-  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const products = getProducts({ limit: 9 });
 
