0856f1b79ef027241b5a1967f1703ac6bc455428
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-10-20 17:27:35 -0700

    chore: upgrade next.js

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,18 @@ import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
 import { type CodeHikeConfig } from 'codehike/mdx';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
+  cacheComponents: true,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   experimental: {
     // Disabled due to font error that's causing them not to be loaded
     // correctly in the browser.
     // inlineCss: true,
-    cacheComponents: true,
     viewTransition: true,
     prerenderEarlyExit: false,
     routerBFCache: true,
   },
-} satisfies NextConfig;
+};
 
 const codeHikeConfig = {
   components: { code: 'MyCode', inlineCode: 'MyInlineCode' },
