d3e154d2d78c6ecc84d9c88077397ce123b1a320
Aurora Scharff <aurora.sofie@gmail.com>
2026-08-25 18:05:42 +0200

    Add prefetch stage API examples

---
+++ app/prefetch-stages/layout.tsx
@@ -0,0 +1,31 @@
+import db from '#/lib/db';
+import { Boundary } from '#/ui/boundary';
+import { Mdx } from '#/ui/codehike';
+import readme from './readme.mdx';
+
+export function generateMetadata() {
+  const demo = db.demo.find({ where: { slug: 'prefetch-stages' } });
+
+  return {
+    title: demo.name,
+    openGraph: { title: demo.name, images: [`/api/og?title=${demo.name}`] },
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Boundary label="Demo" kind="solid" animateRerendering={false}>
+        <Mdx source={readme} collapsed={true} />
+      </Boundary>
+
+      <Boundary
+        label="layout.tsx (statically inferred)"
+        kind="solid"
+        animateRerendering={false}
+      >
+        {children}
+      </Boundary>
+    </>
+  );
+}
